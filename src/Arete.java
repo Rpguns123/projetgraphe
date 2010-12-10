6fbc53859753ba3e438633cdbc39d96192d56f59
thibaut.citron@gmail.com <thibaut.citron@gmail.com@014b17b4-8fef-3437-00eb-56244483a8ba>
2010-12-10 09:03:11 +0000

    MAJ de Jeudi soir 09-12-10

--- src/Arete.java
+++ src/Arete.java
@@ -1,18 +1,35 @@
 
 public class Arete {
 	
-	int s1, s2; // Deux sommets
+	private int s1, s2; // Deux sommets
 	
+	// Constructeur avec parametres
 	public Arete(int x, int y){
 		s1 = x;
 		s2 = y;
 	}
 	
-	public Arete(){ // Constructeur sans parametres
+	// Constructeur sans parametres
+	public Arete(){ 
 		s1 = 0;
 		s2 = 0;
 	}
 	
+	// Fonction d'egalite
+	public boolean isEqual(Arete j){
+		if((s1 == j.s1 && s2 == j.s2)||(s1 == j.s2 && s2 == j.s1))
+			return true;
+		else
+			return false;
+		
+	}
+	
+	// Affichage des de sommets, donc de l'arete
+	public void afficher(){
+		System.out.println("s1 : "+ Integer.toString(s1) + " & s2 : "+Integer.toString(s2));
+	}
+
+	// Setters & Getters des deux sommets, S1 et S2
 	public void setS1(int x){
 		this.s1 = x;
 	}	
